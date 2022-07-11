5ff3f9a581627c6f88d94b7fa0bccbb385c5ac7a
Mariah <mariahschock@gmail.com>
2022-07-11 12:28:38 -0700

    Creating ContextProvider component and wrapping App in ContextProvider in index.js

---
+++ src/ContextProvider.js
@@ -0,0 +1,62 @@
+import { createContext, useState, useContext } from 'react';
+import initialCards from './cards-data';
+
+const CardContext = createContext();
+
+export function ContextProvider({ children }) {
+  const [deck, setDeck] = useState(initialCards);
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [selectedCard, setSelectedCard] = useState();
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
+
+
+  function findCardIndex(value, suit, cards) {
+    return cards.findIndex(card => card.value === value && card.suit === suit);
+  }
+
+  function passCard(card) {
+    const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
+    const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
+
+    // arrays start at zero, but our players start at 1 :shrug:
+    const toHand = playerHands[to - 1] || deck;
+    const fromHand = playerHands[from - 1] || deck;
+
+    const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
+    const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
+
+    const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
+    const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
+
+    toHand.push(cardToMove);
+
+    toSetFunction([...toHand]);
+    fromSetFunction([...fromHand]);
+
+    setSelectedCard(null);
+  }
+
+  const stateAndSetters = {
+    deck, setDeck,
+    playerOneHand, setPlayerOneHand,
+    selectedCard, setSelectedCard,
+    playerTwoHand, setPlayerTwoHand,
+    playerThreeHand, setPlayerThreeHand,
+    from, setFrom,
+    to, setTo,
+    passCard
+  };
+
+  return <CardContext.Provider value={stateAndSetters}>
+    {children}
+  </CardContext.Provider>;
+}
+
+
+
+export function useCardContext() {
+  return useContext(CardContext);
+}
